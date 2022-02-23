1cbb003ddf01c1da65924a9ca999b9156624cfe1
xuchaoqian <chaoranxu@gmail.com>
2022-02-23 10:31:50 +0800

    Demonstrate the bug of creating multi windows

--- create-webview-window/src-tauri/src/main.rs
+++ create-webview-window/src-tauri/src/main.rs
@@ -3,8 +3,13 @@
   windows_subsystem = "windows"
 )]
 
+mod cmds;
+
 fn main() {
   tauri::Builder::default()
+  .invoke_handler(tauri::generate_handler![
+    cmds::create_multi_windows
+  ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- create-webview-window/src/App.tsx
+++ create-webview-window/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { window as tauriWindow } from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/tauri";
 import logo from "./logo.svg";
 import tauriCircles from "./tauri.svg";
 import tauriWord from "./wordmark.svg";
@@ -10,6 +11,7 @@ window.addEventListener("DOMContentLoaded", (event: any) => {
     "click",
     (e: any) => {
       if (e.target.matches("a")) {
+        console.log(e);
         new tauriWindow.WebviewWindow("theUniqueLabel", {
           url: "https://www.apple.com",
         });
@@ -21,6 +23,12 @@ window.addEventListener("DOMContentLoaded", (event: any) => {
     { capture: true }
   );
 });
+
+async function createMultiWindows(): Promise<void> {
+  console.log("hello");
+  return invoke("create_multi_windows");
+}
+
 
 function App() {
   return (
@@ -47,6 +55,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => {createMultiWindows().then((result)=>{console.log(result)});}}>
+        Create multi windows
+      </button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
